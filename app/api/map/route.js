6cda3c9bc3acec99a8fd7057e18bfc01b71e7b3a
KOS1234567890 <fistar02@gmail.com>
2023-10-25 18:09:58 +0900

    20231025 최종본2

--- app/api/map/route.js
+++ app/api/map/route.js
@@ -1,40 +1,35 @@
 import axios from 'axios';
-export async function GET(req){   
-    // 경로탐색 관련 맵
-    const param = req.url.split('?')[1];
-    console.log(param)
-    
+export async function GET(req) {
+	// 경로탐색 관련 맵
+	const param = req.url.split('?')[1];
 
-    const REST_API_KEY = '921e60e895676448579822a2923536f4';
-    const url = `https://apis-navi.kakaomobility.com/v1/directions?${param}`;
-    const map = await axios(url,{
-        headers:{
-            Authorization: `KakaoAK ${REST_API_KEY}`
-        }
-    });
+	// RESTAPI 키 
+	const REST_API_KEY = '59d0e2ff6419b9a8f970c7409a781500';
+	const url = `https://apis-navi.kakaomobility.com/v1/directions?${param}`;
+	const map = await axios(url, {
+		headers: {
+			Authorization: `KakaoAK ${REST_API_KEY}`
+		}
+	});
 
-
-   return Response.json(map.data);
+	return Response.json(map.data);
 }
 
-export async function POST(req){   
-    const d = await req.json();
-    
-    const REST_API_KEY = '921e60e895676448579822a2923536f4';
-    const url = `https://apis-navi.kakaomobility.com/v1/waypoints/directions`;
-    const map = await axios({
-        method:'post',
-        url:url,
-        headers:{
-            "Content-Type": "application/json",
-            "Authorization": `KakaoAK ${REST_API_KEY}`
-        },
-        data : d.loadMap
-    });
+export async function POST(req) {
+	const d = await req.json();
 
-    console.log(map,'=================================================')
+	// RESTAPI 키 
+	const REST_API_KEY = '59d0e2ff6419b9a8f970c7409a781500';
+	const url = `https://apis-navi.kakaomobility.com/v1/waypoints/directions`;
+	const map = await axios({
+		method: 'post',
+		url: url,
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `KakaoAK ${REST_API_KEY}`
+		},
+		data: d.loadMap
+	});
 
-
-   return Response.json(map.data);
-
+	return Response.json(map.data);
 }
